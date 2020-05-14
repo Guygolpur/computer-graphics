ba580c88cd6e084640a2f53b0a87d4f3966a0d8f
Bar Azouri <barazouri24@gmail.com>
2020-05-14 17:07:20 +0300

    move element in window

--- src/Components/draw.js
+++ src/Components/draw.js
@@ -1,66 +1,97 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
 
 class Draw extends Component {
   constructor (props) {
     super(props)
     this.state = {
       left: 0,
-      top: 0
+      top: 0,
+      operation: ''
     }
+    this.handleMoveOperation = this.handleMoveOperation.bind(this);
+
   }
   componentDidMount () {
     document.addEventListener('mousemove', e => {
-      this.setState({ left: e.pageX, top: e.pageY })
+      this.setState({ left: e.pageX - 300, top: e.pageY - 300 }, () => {
+        if (
+          this.state.left > 0 &&
+          this.state.left < 300 &&
+          this.state.top > 0 &&
+          this.state.top < 800 &&
+          this.state.operation === 'move'
+        ) {
+          let cx = document.querySelector('canvas').getContext('2d')
+          cx.clearRect(0, 0, '1000', '1000')
+          this.drawBoat()
+        }
+      })
     })
     this.drawBoat()
-    console.log(this.state.left)
   }
-  bezier = (cp1x, cp1y, cp2x, cp2y, x, y) => {
+  bezier (cp1x, cp1y, cp2x, cp2y, x, y) {
     let cx = document.querySelector('canvas').getContext('2d')
     cx.beginPath()
     cx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
     cx.stroke()
   }
-  drawDiagonal = (x1, y1, x2, y2) => {
+  drawDiagonal (x1, y1, x2, y2) {
     let cx = document.querySelector('canvas').getContext('2d')
     cx.beginPath()
     cx.moveTo(x1, y1)
     cx.lineTo(x2, y2)
     cx.stroke()
   }
-  drawCircle = (x, y, r, start, end) => {
+  drawCircle (x, y, r, start, end) {
     let cx = document.querySelector('canvas').getContext('2d')
     cx.beginPath()
     cx.arc(x, y, r, start, end)
     cx.stroke()
   }
-  drawBoat = () => {
+  drawBoat () {
     const { left, top } = this.state
-    this.bezier(200+left, 10+top, 1000+left, 400+top, 200+left, 400+top)
-    this.drawDiagonal(200, 10, 200, 450)
-    this.drawDiagonal(200, 10, 0, 380)
-    this.drawDiagonal(0, 380, 200, 380)
-    this.drawDiagonal(0, 450, 700, 450)
-    this.drawDiagonal(0, 450, 50, 530)
-    this.drawDiagonal(50, 450, 100, 530)
-    this.drawDiagonal(100, 450, 150, 530)
-    this.drawDiagonal(150, 450, 200, 530)
-    this.drawDiagonal(200, 450, 250, 530)
-    this.drawDiagonal(0, 450, 50, 530)
-    this.drawDiagonal(50, 530, 600, 530)
-    this.drawDiagonal(600, 530, 700, 450)
+    this.bezier(
+      200 + left,
+      10 + top,
+      1000 + left,
+      400 + top,
+      200 + left,
+      400 + top
+    )
+    this.drawDiagonal(200 + left, 10 + top, 200 + left, 450 + top)
+    this.drawDiagonal(200 + left, 10 + top, 0 + left, 380 + top)
+    this.drawDiagonal(0 + left, 380 + top, 200 + left, 380 + top)
+    this.drawDiagonal(0 + left, 450 + top, 700 + left, 450 + top)
+    this.drawDiagonal(0 + left, 450 + top, 50 + left, 530 + top)
+    this.drawDiagonal(50 + left, 450 + top, 100 + left, 530 + top)
+    this.drawDiagonal(100 + left, 450 + top, 150 + left, 530 + top)
+    this.drawDiagonal(150 + left, 450 + top, 200 + left, 530 + top)
+    this.drawDiagonal(200 + left, 450 + top, 250 + left, 530 + top)
+    this.drawDiagonal(0 + left, 450 + top, 50 + left, 530 + top)
+    this.drawDiagonal(50 + left, 530 + top, 600 + left, 530 + top)
+    this.drawDiagonal(600 + left, 530 + top, 700 + left, 450 + top)
 
-    this.drawCircle(600, 490, 20, 0, 7)
-    this.drawCircle(550, 490, 20, 0, 7)
-    this.drawCircle(500, 490, 20, 0, 7)
-    this.drawCircle(450, 490, 20, 0, 7)
+    this.drawCircle(600 + left, 490 + top, 20, 0, 7)
+    this.drawCircle(550 + left, 490 + top, 20, 0, 7)
+    this.drawCircle(500 + left, 490 + top, 20, 0, 7)
+    this.drawCircle(450 + left, 490 + top, 20, 0, 7)
+  }
+  handleMoveOperation () {
+    this.setState({ operation: 'move' })
   }
   render () {
-    console.log(this.state.left)
-
     return (
       <div>
-        <h1 onClick={() => this.drawBoat()}>draw</h1>
+        <div>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={this.handleMoveOperation}
+          >
+            Move
+          </Button>
+        </div>
         <canvas width='1000' height='1000'></canvas>
       </div>
     )
