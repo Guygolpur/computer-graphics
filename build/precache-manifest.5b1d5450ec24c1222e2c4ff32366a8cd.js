18780a447d931ddb90e3cb4cbfc0c930b6c3fa13
bazouri <bazouri@paypal.com>
2020-05-19 17:00:54 +0300

    build

---
+++ build/precache-manifest.5b1d5450ec24c1222e2c4ff32366a8cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee4fac4fecc2166befbfc120c0c8ddf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71928eab84c03a3b9980",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c49f7f0ebc0d5ec9b69",
+    "url": "/static/js/2.e1eb869b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e1eb869b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71928eab84c03a3b9980",
+    "url": "/static/js/main.dcacf96d.chunk.js"
+  },
+  {
+    "revision": "d7d8586b8e7ea562b84f",
+    "url": "/static/js/runtime-main.6d25b00e.js"
+  }
+]);
